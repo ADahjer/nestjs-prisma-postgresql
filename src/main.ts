feb400aff6c0509318df26afcc272bd62f47fd2d
Asmith Dahjer <asmith200205@gmail.com>
2023-11-25 21:07:14 -0500

    enable cors and change global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Add this line
+
+  app.setGlobalPrefix('api/v1'); // Set the global prefix to /api/v1/
+
   await app.listen(3000);
 }
+
 bootstrap();
