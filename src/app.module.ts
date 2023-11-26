a5613672e47c55eba66c22cd657b93c22af0a8cb
Asmith Dahjer <asmith200205@gmail.com>
2023-11-25 20:50:59 -0500

    add prisma to DB management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProjectModule, TaskModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    ProjectModule,
+    TaskModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
